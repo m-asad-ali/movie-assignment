e5eca6f139d590a9502a855f4a22f3ea058ba044
Muhamad Asad <76587731+m-asad-ali@users.noreply.github.com>
2023-11-29 20:10:47 +0500

    routes added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
 
 function App() {
+
   return (
     <>
-      <h1>Movie Assignment</h1>
+      <h1>APP JS</h1>
+      {/* <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:id" component={MovieDetail} />
+        </Switch>
+      </Router> */}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Home from './Pages/Home';
+import MovieDetail from './Pages/MovieDetail';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movie/:id",
+    element: <MovieDetail />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
