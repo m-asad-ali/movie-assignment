16cb2646cbdf1ef8ab3ed3710f3860a3b8f26849
Muhamad Asad <76587731+m-asad-ali@users.noreply.github.com>
2023-11-29 20:16:37 +0500

    initial structure added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
+import Home from './Pages/Home';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-
-
 function App() {
 
   return (
     <>
+      <Home></Home>
       <h1>APP JS</h1>
       {/* <Router>
         <Switch>
--- src/Pages/MovieDetail.js
+++ src/Pages/MovieDetail.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
+
+const movies = [
+    { id: 1, title: 'Movie 1', description: 'Description 1' },
+    { id: 2, title: 'Movie 2', description: 'Description 2' },
+    // Add more movies as needed
+];
 
 const MovieDetail = () => {
-    return (
-        <div>MovieDetail</div>
-    )
-}
+    const { id } = useParams();
+    const movie = movies.find((m) => m.id === parseInt(id, 10));
 
-export default MovieDetail
+    if (!movie) {
+        return <div>Movie not found</div>;
+    }
+
+    return (
+        <div>
+            <Typography variant="h4">{movie.title}</Typography>
+            <Typography variant="body1">{movie.description}</Typography>
+            {/* Add more details as needed */}
+        </div>
+    );
+};
+
+export default MovieDetail;
