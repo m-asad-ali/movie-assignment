eb91a7f1ab83b7c8e6381a30681fdd785b67cb64
Muhamad Asad <76587731+m-asad-ali@users.noreply.github.com>
2023-12-01 19:31:02 +0500

    Searching Functionality Implemented.

--- src/Pages/List.js
+++ src/Pages/List.js
@@ -13,7 +13,16 @@ import { MovieContext } from "../App";
 
 const List = () => {
   const { data, searchTerm } = useContext(MovieContext);
-  console.log("🚀 ~ file: List.js:16 ~ List ~ searchTerm:", searchTerm);
+  let filteredMovies;
+
+  if (searchTerm !== "") {
+    filteredMovies = data.allFilms?.films?.filter((movie) =>
+      movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    console.log("Filter Movie: ", filteredMovies);
+  } else {
+    filteredMovies = [];
+  }
 
   return (
     <>
@@ -22,24 +31,55 @@ const List = () => {
         spacing={3}
         style={{ paddingLeft: "2.5%", paddingRight: "2.5%" }}
       >
-        {data.allFilms.films.map((movie) => (
-          <Grid item key={movie.id} xs={6} sm={4} md={3} lg={3}>
-            <Link to={`/movie/${movie.id}`} style={{ textDecoration: "none" }}>
-              <Card style={{ height: "100%", width: "100%" }}>
-                <CardMedia style={{ height: 300 }} image={img1} />
-                <CardContent>
-                  <Typography
-                    variant="h6"
-                    component="div"
-                    style={{ height: 30, overflow: "hidden" }}
-                  >
-                    {movie.title}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Link>
-          </Grid>
-        ))}
+        {searchTerm === "" ? (
+          data.allFilms.films.map((movie) => (
+            <Grid item key={movie.id} xs={6} sm={4} md={3} lg={3}>
+              <Link
+                to={`/movie/${movie.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Card style={{ height: "100%", width: "100%" }}>
+                  <CardMedia style={{ height: 300 }} image={img1} />
+                  <CardContent>
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      style={{ height: 30, overflow: "hidden" }}
+                    >
+                      {movie.title}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Link>
+            </Grid>
+          ))
+        ) : filteredMovies.length !== 0 ? (
+          filteredMovies.map((movie) => (
+            <Grid item key={movie.id} xs={6} sm={4} md={3} lg={3}>
+              <Link
+                to={`/movie/${movie.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Card style={{ height: "100%", width: "100%" }}>
+                  <CardMedia style={{ height: 300 }} image={img1} />
+                  <CardContent>
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      style={{ height: 30, overflow: "hidden" }}
+                    >
+                      {movie.title}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Link>
+            </Grid>
+          ))
+        ) : (
+          <Typography variant="h6" gutterBottom>
+            NO MOVIE FOUND
+          </Typography>
+        )}
       </Grid>
     </>
   );
